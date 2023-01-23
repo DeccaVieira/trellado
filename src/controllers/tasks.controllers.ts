d5b3e6abe75fc53ba75600f17d5cd63c103e4d3f
Andreia Vieira <andreiavieira2353@gmail.com>
2023-01-22 23:33:11 -0300

    Get tasks by status

--- src/controllers/tasks.controllers.ts
+++ src/controllers/tasks.controllers.ts
@@ -1,9 +1,23 @@
 import {Request, Response} from 'express';
 import tasksRepositories from "../repository/tasks.repository.js"
 import taskSchema from "../models/tasks.schema.js"
+import { string } from '../../node_modules/joi/lib/index.js';
 
-export function getTasks(req: Request, res:Response){
+export async function getTasks(req: Request, res:Response){
   const {status} = req.query;
+  
+  try{
+    if(status){
+      const task = await tasksRepositories.getTasksByStatus(status);
+      
+      //return res.sendStatus(200) ;
+      
+      console.log(task);
+   return res.send(task.rows)
+}
+  }catch (err) {
+    return res.status(400).send(err);
+  }
 
 
 
